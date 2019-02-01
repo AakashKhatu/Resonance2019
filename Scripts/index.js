83f457ba6b338dc1dffc8c82d8b3ed7b6895cff2
Aakash Khatu <aakashkhatu1998@gmail.com>
2019-02-01 23:12:27 +0530

    added random images and completed cut corners

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,6 +2,11 @@ const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
+var randomimages = Array.from(document.getElementsByTagName('img'));
+randomimages.forEach(function ( x, i ){
+  x.src = x.src + "&" + i;
+})
+
 var l = Array.from(document.getElementsByClassName('list-head'));
 l.forEach(function( elem ){elem.addEventListener('click', async function(){
   var x = elem.nextElementSibling;
