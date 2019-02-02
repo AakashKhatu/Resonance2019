da620759bf229310d64fb67bcfea774e704eaefe
Aakash Khatu <aakashkhatu1998@gmail.com>
2019-02-02 11:27:11 +0530

    added base animations

--- Scripts/index.js
+++ Scripts/index.js
@@ -13,3 +13,13 @@ l.forEach(function( elem ){elem.addEventListener('click', async function(){
   x.classList.toggle("hide");
   x.classList.toggle("shrink");
 })});
+
+function findFirstPositive(b, a, i, c) {
+  c=(d,e)=>e>=d?(a=d+(e-d)/2,0<b(a)&&(a==d||0>=b(a-1))?a:0>=b(a)?c(a+1,e):c(d,a-1)):-1
+  for (i = 1; 0 >= b(i);) i *= 2
+  return c(i / 2, i)|0
+}
+
+var dpi = findFirstPositive(x => matchMedia(`(max-resolution: ${x}dpi)`).matches)
+
+console.log(dpi)
