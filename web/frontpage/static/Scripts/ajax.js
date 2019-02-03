eb4ca8f010db95bdcfea73502a9b3c879ba09918
Aakash Khatu <aakashkhatu1998@gmail.com>
2019-02-03 22:05:04 +0530

    content overlay complete

--- web/frontpage/static/Scripts/ajax.js
+++ web/frontpage/static/Scripts/ajax.js
@@ -1,5 +1,16 @@
 var xhr = new XMLHttpRequest();
 var jsondata, interEvents, intraEvents;
+var na = document.getElementById("na");
+var image = document.getElementById("image");
+var description = document.getElementById("description");
+var entryfee = document.getElementById("entryfee");
+var prize = document.getElementById("prize");
+var members = document.getElementById("members");
+var date = document.getElementById("date");
+var starttime = document.getElementById("starttime");
+var endtime = document.getElementById("endtime");
+var loc = document.getElementById("location");
+
 xhr.open('GET', '../returnJson/');
 xhr.send();
 
@@ -8,3 +19,29 @@ xhr.onload = function () {
   interEvents = JSON.parse(json.inter);
   intraEvents = JSON.parse(json.intra);
 };
+
+function fillOverlay( ce ){
+  var even, s;
+  id = ce.id
+  if(id[3] == 'r')
+    s = intraEvents;
+  else
+    s = interEvents;
+  pk = id[5];
+  for (var key in s) {
+    if(s[key].pk == pk){
+        even = s[key].fields;
+        break;
+    }
+  }
+  na.innerHTML = even["name"];
+  image.innerHTML = even["image"];
+  description.innerHTML = even["description"];
+  entryfee.innerHTML = even["entry_fee"];
+  prize.innerHTML = even["prize"];
+  members.innerHTML = even["members"];
+  date.innerHTML = even["date"];
+  starttime.innerHTML = even["starttime"];
+  endtime.innerHTML = even["endtime"];
+  loc.innerHTML = even["location"];
+}
