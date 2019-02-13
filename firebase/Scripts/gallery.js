3b85242aab69a9ce6b52fb11709d58cd8965dc30
zer0k <aakashkhatu1998@gmail.com>
2019-02-13 15:22:42 +0530

    updated descriptions

--- firebase/Scripts/gallery.js
+++ firebase/Scripts/gallery.js
@@ -3,30 +3,118 @@ function openPhotoSwipe() {
 
   // build items array
   var items = [{
-      src: 'https://placekitten.com/600/400',
-      w: 600,
-      h: 400
-    },
-    {
-      src: 'https://placekitten.com/1200/900',
-      w: 1200,
-      h: 900
-    },
-    {
-      src: 'https://placekitten.com/800/600',
-      w: 800,
-      h: 600
-    },
-    {
-      src: 'https://placekitten.com/800/400',
-      w: 800,
-      h: 400
-    },
+	src: 'Media/prev/1.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/3.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/4.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/5.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/6.jpg',
+	w: 800,
+	h: 1200
+	},
+{
+	src: 'Media/prev/7.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/8.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/9.jpg',
+	w: 800,
+	h: 1200
+	},
+{
+	src: 'Media/prev/10.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/11.jpg',
+	w: 800,
+	h: 1200
+	},
+{
+	src: 'Media/prev/12.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/13.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/14.jpg',
+	w: 800,
+	h: 1200
+	},
+{
+	src: 'Media/prev/15.jpg',
+	w: 800,
+	h: 600
+	},
+{
+	src: 'Media/prev/16.jpg',
+	w: 800,
+	h: 600
+	},
+{
+	src: 'Media/prev/17.jpg',
+	w: 800,
+	h: 1067
+	},
+{
+	src: 'Media/prev/18.jpg',
+	w: 800,
+	h: 600
+	},
+{
+	src: 'Media/prev/19.jpg',
+	w: 800,
+	h: 600
+	},
+{
+	src: 'Media/prev/20.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/21.jpg',
+	w: 800,
+	h: 1200
+	},
+{
+	src: 'Media/prev/22.jpg',
+	w: 800,
+	h: 533
+	},
+{
+	src: 'Media/prev/23.jpg',
+	w: 800,
+	h: 533
+	},
   ];
   var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
+    index: 0
   };
 
   // Initializes and opens PhotoSwipe
