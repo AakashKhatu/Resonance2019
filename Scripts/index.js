48f91c6814b12cb5449e7efaf1c362a147e86ff8
zer0k <aakashkhatu1998@gmail.com>
2019-02-21 12:36:06 +0530

    deleted django and added avengers font

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,10 +1,21 @@
 var ovset = false;
 var listitems = Array.from(document.getElementsByTagName('li'));
+var elemid="not updated";
+var xhr = new XMLHttpRequest();
+// document.getElementById("form").width = document.clientWidth;
+var currentForm = "";
+xhr.onload = function () {
+  ov.children[1].innerHTML = xhr.responseText;
+};
 listitems.forEach(function(lElem) {
   if (lElem.parentElement.classList.contains("eventslist")){
   lElem.setAttribute('data-aos', "fade-left");
   lElem.setAttribute('data-aos-easing', "ease");
   lElem.addEventListener('click', function() {
+    elem = this
+    xhr.open('GET', './descriptions/'+this.id+'.txt');
+    xhr.send();
+    currentForm = this.getAttribute('data-scope');
     if (ovset) {
       ov.classList.toggle("closed");
       ov.classList.toggle("opened");
@@ -37,8 +48,6 @@ function findFirstPositive(b, a, i, c) {
 
 var dpi = findFirstPositive(x => matchMedia(`(max-resolution: ${x}dpi)`).matches)
 
-console.log(dpi * 4)
-
 var sheet = document.createElement('style')
 sheet.innerHTML = ".eventslist > li {width: " + dpi * 4.3 + "px;}";
 document.body.appendChild(sheet);
@@ -49,3 +58,57 @@ clov.addEventListener('click', function() {
   ov.classList.toggle("closed");
   ov.classList.toggle("opened");
 });
+var regset = false;
+var clreg = document.getElementById('regcl');
+var reg = document.getElementById('reg');
+var iform = document.getElementById('form');
+
+clreg.addEventListener('click', function() {
+    iform.hidden = !iform.hidden;
+  reg.classList.toggle("closed");
+  reg.classList.toggle("opened");
+});
+
+var interform="https://goo.gl/forms/4WYVjs4jYpQ8C90V2";
+var intraform="https://goo.gl/forms/H60xokGc1aleBm5c2";
+var edmform="https://goo.gl/forms/nm0yK7IGTtxMUGF82";
+
+function openRegister(t) {
+  if(t.getAttribute('id') == "registrationlink"){
+    if(currentForm == "inter"){
+      if(iform.src!=interform)
+        iform.src=interform;
+    }
+    else if(currentForm == "intra"){
+      if(iform.src!=intraform)
+        iform.src=intraform;
+    }
+  }
+  else{
+    if(iform.src!=edmform)
+        iform.src=edmform;
+  }
+  iform.hidden = !iform.hidden;
+  if (regset) {
+    reg.classList.toggle("closed");
+    reg.classList.toggle("opened");
+  } else {
+    reg.classList.toggle("opened");
+    regset = true;
+  }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
